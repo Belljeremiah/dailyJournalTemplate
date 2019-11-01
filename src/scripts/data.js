28bc272cd2ecb7ac82ce9c9c618b83e23f1957da
Jeremiah Bell <jeremiah.bell812.topper@gmail.com>
2019-11-01 14:43:28 -0600

    partially through chapter 8 need to work from the place above challenges

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -3,14 +3,18 @@ const URLforAPI = "http://localhost:3000/entries";
 
 
 
+// THis is where I declare a const or variable for the API functions I am using to getjournalEntries and to saveJournalEntries
 
 const API = {
  
+  // This is my getJournal Entries fetch call. It returns the fetch of the API local and then parses the response into json for me and calls itself.
   getJournalEntries () {
       return fetch(URLforAPI)
           .then(response => response.json())
   },
   
+  // This is a saveJOurnalEntry fetch and object that I made using code from the examples and steve Brownlee and Kristen Norris' Lessons
+  // IT takes an argument of entry and then returns a fetch and enacts the method POST to post onto the dom IT also makes the JSON response into a string for Entry
   saveJournalEntry (entry) {
     return fetch(URLforAPI, {
       method: "POST",
